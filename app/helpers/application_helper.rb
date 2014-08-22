aee1aa34d4dbbf6392cc82f7f4bbdfaa9a2ab490
PRAVEEN ARAVAMUDHAM <praveen.aravamudham@gmail.com>
2014-08-22 13:55:22 -0400

    Finishing static pages layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# returns a full title for every page on the website
+	def full_page_title(page_title)
+		base_title = "Ruby on Rails tutorials"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 	describe "Static pages" do
+		let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   		
   		describe "Home page" do
     		it "should have the content 'Sample App'" do
-      			visit '/static_pages/home'
+      			visit home_path
       			expect(page).to have_content('Sample App')
 			end
 
 			it "should have the right title" do
 				visit '/static_pages/home'
-				expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+				expect(page).to have_title("#{base_title} | Home")
 			end 
 		end
 
@@ -21,7 +22,7 @@ require 'spec_helper'
 
 			it "should have the right title" do
 				visit '/static_pages/help'
-				expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+				expect(page).to have_title("#{base_title} | Help")
 			end  
 		end
 
@@ -33,9 +34,22 @@ require 'spec_helper'
 
 			it "should have the right title" do
 				visit '/static_pages/about'
-				expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+				expect(page).to have_title("#{base_title} | About")
 			end 
 		end
+
+		describe "Contact page" do
+    		it "should have the content 'Contact'" do
+      			visit '/static_pages/contact'
+      			expect(page).to have_content('Contact')
+			end 
+
+			it "should have the right title" do
+				visit '/static_pages/contact'
+				expect(page).to have_title("#{base_title} | Contact")
+			end 
+		end
+
 
 
 	end
